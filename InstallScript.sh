39cad2b6d891addc53d326fb7abc38c54e6380d9
Jakub Sobotka <84909372+Jacobsob@users.noreply.github.com>
2022-11-04 13:24:19 +0100

    add installScript (#1)
    * add installScript
    
    * add switch to type skeleton/tutorial
    
    * add readme

---
+++ InstallScript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+declare type=$1
+[ -z "$type" ] && type='skeleton'
+[ "$type" != "skeleton" ] && [ "$type" != "tutorial" ] && echo Only skeleton or tutorial is allowed && exit
+read -p 'Project name: ' projectName
+[ -z "$projectName" ] && echo Error: Project name must be specified && exit
+read -p 'Output (Default: ./): ' output
+[ -z "$output" ] && declare output='./'
+mkdir -p $output
+[ -d "$output" ] && echo $output was successfully created || (echo $output was not created && exit)
+git clone https://github.com/Orchesty/orchesty-"$type".git $output/$projectName
+echo Orchesty was successfully downloaded
+cd $output/$projectName
+rm -rf .git
+sed -i 's/skeleton/'${projectName,,}'/g' .env.dist >> .env.dist
+sed -i 's/Skeleton/'${projectName[@]^}'/' README.md >> README.md
+sed -i 's/skeleton/'${projectName,,}'/g' nodejs-sdk/package.json >> nodejs-sdk/package.json
+sed -i 's/skeleton/'${projectName,,}'/g' php-sdk/composer.json >> php-sdk/composer.json
+echo Project name: [$projectName] was created to directory: $output
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Installer
+Installer is script for fast create your own integration project.
+
+## Prerequisites
+- Bash
+
+## How to use
+- Script run via bash.
+- You can use parameter to select type of project (skeleton or tutorial).
+
+### Arguments (interactive)
+- Project name: Enter what you want the project to be named. (required)
+- Output: Enter where you want the project to be created. (optional)
+
+### Orchesty (default)
+```
+bash InstallScript.sh
+```
+or 
+```
+bash InstallScript.sh skeleton
+```
+
+### Orchesty (with tutorial codes)
+```
+bash InstallScript.sh tutorial
+```
+
+## Sample
+```
+bash InstallScript.sh
+Project name: MyOwnProject [ENTER]
+Output (Default: ./): [ENTER]
+...
+Orchesty was successfully downloaded
+Project name: [MyOwnProject] was created to directory: ./
+```
+
+- Now your project is created in ./ (actual directory in terminal)
+
