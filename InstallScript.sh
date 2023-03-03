b700f485cf0be67794bad8b05645d4e86275e1b6
Radek Jirsa <jirsa.r@ghanaboso.com>
2023-03-03 10:26:11 +0100

    Updated sed syntax

--- InstallScript.sh
+++ InstallScript.sh
@@ -12,8 +12,8 @@ git clone https://github.com/Orchesty/orchesty-"$type".git $output/$projectName
 echo Orchesty was successfully downloaded
 cd $output/$projectName
 rm -rf .git
-sed -i 's/skeleton/'${projectName,,}'/g' .env.dist >> .env.dist
+sed -i 's/skeleton/'$projectName'/g' .env.dist >> .env.dist
 sed -i 's/Skeleton/'${projectName[@]^}'/' README.md >> README.md
-sed -i 's/skeleton/'${projectName,,}'/g' nodejs-sdk/package.json >> nodejs-sdk/package.json
-sed -i 's/skeleton/'${projectName,,}'/g' php-sdk/composer.json >> php-sdk/composer.json
+sed -i 's/skeleton/'$projectName'/g' nodejs-sdk/package.json >> nodejs-sdk/package.json
+sed -i 's/skeleton/'$projectName'/g' php-sdk/composer.json >> php-sdk/composer.json
 echo Project name: [$projectName] was created to directory: $output
